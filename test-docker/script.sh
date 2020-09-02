4d8615875348562926105bd83c690afd4be31b6a
Marc <marc.romanens@swissdotnet.ch>
2020-09-02 14:18:14 +0200

    Enh: Add minimal docker example who's failing

---
+++ test-docker/script.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cd test-1/
+docker build . -t test-1
+cd ..
+
+cd test-2
+docker build . -t test-2
+cd ..
+
+cd test-3
+docker build . -t test-3
+cd ..
+
+cd test-4
+docker build . -t test-4
+cd ..
+
+cd test-5
+docker build . -t test-5
+cd ..
+
+cd test-6
+docker build . -t test-6
+cd ..
+
+cd test-7
+docker build . -t test-7
+cd ..
+
+docker service create --name test-1 test-1
+docker service create --name test-2 test-2
+docker service create --name test-3 test-3
+docker service create --name test-4 test-4
+docker service create --name test-5 test-5
+docker service create --name test-6 test-6
+docker service create --name test-6 test-7
