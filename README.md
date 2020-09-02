2382a39f364c702d6b3d9f0ecf1a653d3b5f7bd9
Marc <marc.romanens@swissdotnet.ch>
2020-09-02 14:54:41 +0200

    Add some tests

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # docker-healthcheck-issue
+
+This repository is just a helper to deploy multiples scenarios about this issue : 
+
+
+## Usage
+```
+cd test-docker
+./scripts.sh
+
+watch -n 1 "docker container ls  | grep test"
+```
--- test-docker/script.sh
+++ test-docker/script.sh
@@ -28,10 +28,20 @@ cd test-7
 docker build . -t test-7
 cd ..
 
+cd test-8
+docker build . -t test-8
+cd ..
+
+cd test-no-healthcheck
+docker build . -t test-no-healthcheck
+cd ..
+
 docker service create --name test-1 test-1
 docker service create --name test-2 test-2
 docker service create --name test-3 test-3
 docker service create --name test-4 test-4
 docker service create --name test-5 test-5
 docker service create --name test-6 test-6
-docker service create --name test-6 test-7
+docker service create --name test-7 test-7
+docker service create --name test-8 test-8
+docker service create --name test-no-healthcheck test-no-healthcheck
